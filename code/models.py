af60c4c611b433a1aaf32cbd7a298e89186bd305
FGDKB <1805240845@qq.com>
2025-04-18 08:45:40 +0800

    commit

--- code/models.py
+++ code/models.py
@@ -1,4 +1,4 @@
-from torch_geometric.nn import HGTConv, Linear
+from torch_geometric.nn import Linear
 from sklearn.model_selection import train_test_split, ParameterGrid
 from sklearn.metrics import roc_auc_score
 import numpy as np
@@ -6,8 +6,86 @@ import joblib
 from lightgbm import LGBMClassifier
 from utils import get_metrics, get_name_list, plot_curves
 import torch
+import torch.nn.functional as F
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+class CustomHGTConv(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, metadata, num_heads=1, dropout=0.2):
+        super().__init__()
+        
+        self.metadata = metadata
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.num_heads = num_heads
+        self.dropout = dropout
+        
+        self.linear_relations = torch.nn.ModuleDict()
+        
+        node_types, edge_types = metadata[0], metadata[1]
+        
+        for edge_type in edge_types:
+            src, edge, dst = edge_type
+            
+            self.linear_relations[f"{src}_{edge}_{dst}_k"] = Linear(-1, out_channels * num_heads)
+            self.linear_relations[f"{src}_{edge}_{dst}_q"] = Linear(-1, out_channels * num_heads)
+            self.linear_relations[f"{src}_{edge}_{dst}_v"] = Linear(-1, out_channels * num_heads)
+            
+            self.linear_relations[f"{src}_{edge}_{dst}_a"] = Linear(out_channels * num_heads, num_heads)
+        
+        self.linear_out = torch.nn.ModuleDict()
+        for node_type in node_types:
+            self.linear_out[node_type] = Linear(out_channels * num_heads, out_channels)
+    
+    def forward(self, x_dict, edge_index_dict):
+        result_dict = {node_type: [] for node_type in x_dict.keys()}
+        
+        for edge_type, edge_index in edge_index_dict.items():
+            src_type, edge_type_name, dst_type = edge_type
+            
+            src_nodes = x_dict[src_type]
+            dst_nodes = x_dict[dst_type]
+            
+            row, col = edge_index
+            
+            src_features = src_nodes[row]
+            dst_features = dst_nodes[col]
+            
+            k = self.linear_relations[f"{src_type}_{edge_type_name}_{dst_type}_k"](src_features)
+            q = self.linear_relations[f"{src_type}_{edge_type_name}_{dst_type}_q"](dst_features)
+            v = self.linear_relations[f"{src_type}_{edge_type_name}_{dst_type}_v"](src_features)
+            
+            k = k.view(-1, self.num_heads, self.out_channels)
+            q = q.view(-1, self.num_heads, self.out_channels)
+            v = v.view(-1, self.num_heads, self.out_channels)
+            
+            alpha = (k * q).sum(dim=-1) / (self.out_channels ** 0.5)
+            alpha = F.leaky_relu(alpha, 0.2)
+            
+            alpha = F.softmax(alpha, dim=0)
+            alpha = F.dropout(alpha, p=self.dropout, training=self.training)
+            
+            alpha = alpha.unsqueeze(-1)
+            
+            out = v * alpha
+            
+            out = out.reshape(-1, self.num_heads * self.out_channels)
+            
+            out_dst = torch.zeros(dst_nodes.size(0), self.num_heads * self.out_channels, device=out.device)
+            out_dst.scatter_add_(0, col.view(-1, 1).expand(-1, self.num_heads * self.out_channels), out)
+            
+            out_dst = self.linear_out[dst_type](out_dst)
+            
+            result_dict[dst_type].append(out_dst)
+        
+        for node_type, features_list in result_dict.items():
+            if features_list:
+                result_dict[node_type] = torch.stack(features_list).mean(dim=0)
+            else:
+                result_dict[node_type] = x_dict[node_type]
+        
+        return result_dict
 
 
 class HGT(torch.nn.Module):
@@ -22,7 +100,7 @@ class HGT(torch.nn.Module):
 
         self.convs = torch.nn.ModuleList()
         for _ in range(num_layers):
-            conv = HGTConv(-1, hidden_channels, data.metadata(), num_heads)
+            conv = CustomHGTConv(-1, hidden_channels, data.metadata(), num_heads)
             self.convs.append(conv)
 
         self.fc = Linear(hidden_channels * 2, 2)
@@ -67,60 +145,86 @@ class HGT(torch.nn.Module):
         return predictions
 
     def save_model_state(self, kf, train_idx, test_idx, y):
-
-
+        self._store_indices_and_labels(train_idx, test_idx, y)
+        
+        self._process_and_save_features(kf)
+    
+    def _store_indices_and_labels(self, train_idx, test_idx, y):
         self.train_idx = train_idx
         self.test_idx = test_idx
         self.y = y
-
-        self.concat_same_m_d(kf)
-
-        with torch.no_grad():
-            out = self(self.data_temp, self.edg_index_all)
-            y_pred = out[self.test_idx].cpu().numpy().flatten()
-            joblib.dump(y_pred, f'HMDAD/mid_data_HMDAD/{6}nl{kf}kf_y_pred.dict')
-
-    def concat_same_m_d(self, kf):
-
+    
+    def _process_and_save_features(self, kf):
         data_concat = torch.concat(
             (self.save_data['n1'][self.edge_index[0]], self.save_data['n2'][self.edge_index[1]]),
             dim=1
         ).cpu().numpy()
-
+        
         train_data_concat = data_concat[self.train_idx]
         test_data_concat = data_concat[self.test_idx]
-
+        
         with torch.no_grad():
             out = self(self.data_temp, self.edg_index_all)
             y_test_pred = out[self.test_idx].cpu().numpy().flatten()
-
-        joblib.dump({
-            'train_data': train_data_concat,
-            'test_data': test_data_concat,
-            'y_train': self.y[self.train_idx].cpu().numpy(),
-            'y_test': self.y[self.test_idx].cpu().numpy(),
-            'y_test_pred': y_test_pred,
-            'all_data': {
-                'Em': self.save_data['n1'].cpu().numpy(),
-                'Ed': self.save_data['n2'].cpu().numpy()
-            },
-        }, f'HMDAD/mid_data_HMDAD/{6}nl{kf}kf_best_cat_data.dict')
+        
+        # 准备数据
+        train_data = {
+            'features': train_data_concat,
+            'labels': self.y[self.train_idx].cpu().numpy()
+        }
+        
+        test_data = {
+            'features': test_data_concat,
+            'labels': self.y[self.test_idx].cpu().numpy(),
+            'predictions': y_test_pred
+        }
+        
+        node_embeddings = {
+            'Em': self.save_data['n1'].cpu().numpy(),
+            'Ed': self.save_data['n2'].cpu().numpy()
+        }
+        
+        # 保存所有数据到文件
+        output_path = f'HMDAD/mid_data_HMDAD/{6}nl{kf}kf_best_cat_data.dict'
+        save_data = {
+            'train_data': train_data['features'],
+            'test_data': test_data['features'],
+            'y_train': train_data['labels'],
+            'y_test': test_data['labels'],
+            'y_test_pred': test_data['predictions'],
+            'all_data': node_embeddings
+        }
+        
+        joblib.dump(save_data, output_path)
+    
+    def concat_same_m_d(self, kf):
+        self._process_and_save_features(kf)
 
 
 class ModelSelector:
 
 
-    def __init__(self):
-
-        self.models = {
-            'lightgbm': LGBMClassifier(),
+    def __init__(self, custom_models=None, custom_params=None):
+        default_models = {
+            'lightgbm': LGBMClassifier()
         }
-
-        self.param_grids = {
-            'lightgbm': {'num_leaves': [25], 'learning_rate': [0.15], 'max_depth':[-1]},
-
+        
+        default_param_grids = {
+            'lightgbm': {
+                'num_leaves': [25], 
+                'learning_rate': [0.15], 
+                'max_depth': [-1]
+            }
         }
-
+        
+        self.models = default_models.copy()
+        if custom_models:
+            self.models.update(custom_models)
+            
+        self.param_grids = default_param_grids.copy()
+        if custom_params:
+            self.param_grids.update(custom_params)
+    
     def get_models(self, model_list=[]):
 
         if not model_list:
@@ -151,7 +255,7 @@ class ModelSelector:
 
                 model.fit(X_train, y_train)
 
-                y_score = model.predict_proba(X_test)[:, 1]  # 获取正类概率
+                y_score = model.predict_proba(X_test)[:, 1]
 
 
                 auc = roc_auc_score(y_test, y_score)
@@ -207,7 +311,6 @@ class ModelSelector:
         return results
 
     def calculate_average_performance(self, all_fold_results):
-
 
         all_models = set()
         for fold_result in all_fold_results:
